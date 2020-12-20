a19aed5632f194e676943e194eda6632d68e2b20
Alain D'Ettorre <alain.det@gmail.com>
2020-12-20 16:59:47 +0100

    Change fs.writeFileSync to fs.writeFile

--- first-server/app.js
+++ first-server/app.js
@@ -32,16 +32,16 @@ const server = http.createServer((req, res) => {
       console.log('chunk', chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();  
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();  
   }
-
 });
 
 server.listen(3000);

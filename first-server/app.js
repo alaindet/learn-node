de0d44280a1ad0b72a26b45e9e0b45e0b6e85c88
Alain D'Ettorre <alain.det@gmail.com>
2020-12-20 14:20:11 +0100

    Setup mutliple routes

--- first-server/app.js
+++ first-server/app.js
@@ -1,16 +1,46 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-  res.setHeader('Content-Type', 'text/html');
+  const url = req.url;
+  const method = req.method;
 
-  [
-    '<html>',
-    '<head><title>Node.js demo</title></head>',
-    '<body>Hello world</body>',
-    '</html>',
-  ].map(line => res.write(line));
-  res.end();
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    [
+      '<html>',
+      '<head><title>Node.js demo</title></head>',
+      '<body>',
+        '<form action="/message" method="POST">',
+          '<input ',
+            'type="text" ',
+            'name="message" ',
+            'placeholder="Enter a message..." ',
+          '>',
+          '<button type="submit">Submit</button>',
+        '</form>',
+      '</body>',
+      '</html>',
+    ].map(line => res.write(line));
+    return res.end();
+  }
+
+  if (method === 'POST' && url === '/message') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log('chunk', chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();  
+  }
 
 });
 

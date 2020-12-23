9759fcc544bb6529903ba55f74de3d459aff503e
Alain D'Ettorre <alain.det@gmail.com>
2020-12-23 03:07:45 +0100

    Add Order entity

--- udemy-academind/7.mysql/util/database-init.js
+++ udemy-academind/7.mysql/util/database-init.js
@@ -2,6 +2,8 @@ const Product = require('../models/product');
 const User = require('../models/user');
 const Cart = require('../models/cart');
 const CartItem = require('../models/cart-item');
+const Order = require('../models/order');
+const OrderItem = require('../models/order-item');
 
 const initUserIfNone = () => {
   return User.findByPk(1) // TODO: hardcoded value
@@ -37,6 +39,9 @@ const initAssociations = () => {
   Cart.belongsTo(User);
   Cart.belongsToMany(Product, { through: CartItem });
   Product.belongsToMany(Cart, { through: CartItem });
+  Order.belongsTo(User);
+  User.hasMany(Order);
+  Order.belongsToMany(Product, { through: OrderItem });
 };
 
 module.exports = {

a0f9da6c255b9e9a043d1e5a7e81b06f04548412
Alain D'Ettorre <alain.det@gmail.com>
2021-07-13 07:20:46 +0200

    WIP: Add controller decorator

--- validation-demo/src/server.ts
+++ validation-demo/src/server.ts
@@ -35,3 +35,41 @@ app.use(errorHandler);
 // Bootstrap
 const PORT = 3000;
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+
+// THIS works
+/*
+interface Route {
+  path: string;
+  method: 'get' | 'post' | 'put' | 'delete'; // Add the rest...
+}
+
+class BaseController {
+  prefix: string;
+  routes: Route[] = [];
+}
+
+function Controller(prefix = '/') {
+  console.log('Controller startup');
+  return function <T extends { new(...args: any[]): {} }>(constructor: T) {
+    console.log('Controller runtime');
+    constructor.prototype.prefix = prefix;
+  }
+}
+
+@Controller('/foo')
+class FooController extends BaseController {
+
+  // @Route('POST', '/create')
+  create(): void {
+    console.log('FooController.create');
+  }
+
+  // @Route('GET', '/read')
+  read(): void {
+    console.log('FooController.read');
+  }
+}
+
+const foo = new FooController();
+console.log(foo.prefix);
+*/
